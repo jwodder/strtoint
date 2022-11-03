3054f79abeb448a72af3bf41b1fa111da9419bd8
John T. Wodder II <git@varonathe.org>
2022-11-03 15:33:51 -0400

    Got something that compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use num_traits::sign::Signed;
-use num_traits::int::PrimInt;
+use core::num::ParseIntError;
 use num_traits::Num;
 
 pub trait ParseInt {
@@ -10,21 +9,14 @@ pub trait ParseInt {
         Self: Sized;
 }
 
-impl<N: PrimInt + Signed> ParseInt for N {
-    type Err = <Self as Num>::FromStrRadixErr;
+impl ParseInt for i32 {
+    type Err = ParseIntError;
 
     fn parse_int(s: &str) -> Result<Self, Self::Err>
     where
         Self: Sized,
     {
-        let (s, is_pos) = extract_sign(s);
-        let (s, radix) = extract_radix(s);
-        if s.starts_with(['+', '-']) {
-            // Sign after radix prefix; force an appropriate error by parsing
-            // a string with a sign in the middle:
-            return <Self as Num>::from_str_radix("0+", 10);
-        }
-        let x = <Self as Num>::from_str_radix(s, radix)?;
+        let (x, is_pos) = scan_int_str(s)?;
         if is_pos {
             Ok(x)
         } else {
@@ -35,54 +27,59 @@ impl<N: PrimInt + Signed> ParseInt for N {
     }
 }
 
-impl<N: PrimInt + !Signed> ParseInt for N {
-    type Err = <Self as Num>::FromStrRadixErr;
+impl ParseInt for u32 {
+    type Err = ParseIntError;
 
     fn parse_int(s: &str) -> Result<Self, Self::Err>
     where
         Self: Sized,
     {
-        let (s, is_pos) = extract_sign(s);
+        let (x, is_pos) = scan_int_str(s)?;
         if !is_pos {
             // Parsing a negative number as an unsigned number; force an
             // appropriate error:
             return <Self as Num>::from_str_radix("-1", 10);
         }
-        let (s, radix) = extract_radix(s);
-        if s.starts_with(['+', '-']) {
-            // Sign after radix prefix; force an appropriate error by parsing
-            // a string with a sign in the middle:
-            return <Self as Num>::from_str_radix("0+", 10);
-        }
-        <Self as Num>::from_str_radix(s, radix)
+        Ok(x)
     }
 }
 
-fn extract_sign(s: &str) -> (&str, bool) {
+fn scan_int_str<N: Num>(s: &str) -> Result<(N, bool), <N as Num>::FromStrRadixErr> {
     // bool is true for positive, false for negative
-    if let Some(t) = s.strip_prefix("+") {
-        (t, true)
-    } else if let Some(t) = s.strip_prefix("-") {
-        (t, false)
-    } else {
-        (s, true)
+    let (s, is_pos) = {
+        if let Some(t) = s.strip_prefix('+') {
+            (t, true)
+        } else if let Some(t) = s.strip_prefix('-') {
+            (t, false)
+        } else {
+            (s, true)
+        }
+    };
+    let (s, radix) = {
+        if let Some(t) = s.strip_prefix("0x") {
+            (t, 16)
+        } else if let Some(t) = s.strip_prefix("0X") {
+            (t, 16)
+        } else if let Some(t) = s.strip_prefix("0o") {
+            (t, 8)
+        } else if let Some(t) = s.strip_prefix("0O") {
+            (t, 8)
+        } else if let Some(t) = s.strip_prefix("0b") {
+            (t, 2)
+        } else if let Some(t) = s.strip_prefix("0B") {
+            (t, 2)
+        } else {
+            (s, 10)
+        }
+    };
+    if s.starts_with(['+', '-']) {
+        // Sign after radix prefix; force an appropriate error by parsing a
+        // string with a sign in the middle:
+        match <N as Num>::from_str_radix("0+", 10) {
+            Err(e) => return Err(e),
+            _ => unreachable!(),
+        }
     }
-}
-
-fn extract_radix(s: &str) -> (&str, u32) {
-    if let Some(t) = s.strip_prefix("0x") {
-        (t, 16)
-    } else if let Some(t) = s.strip_prefix("0X") {
-        (t, 16)
-    } else if let Some(t) = s.strip_prefix("0o") {
-        (t, 8)
-    } else if let Some(t) = s.strip_prefix("0O") {
-        (t, 8)
-    } else if let Some(t) = s.strip_prefix("0b") {
-        (t, 2)
-    } else if let Some(t) = s.strip_prefix("0B") {
-        (t, 2)
-    } else {
-        (s, 10)
-    }
+    let x = <N as Num>::from_str_radix(s, radix)?;
+    Ok((x, is_pos))
 }
