c51b24f89bfaf11d3353b3880566aee24d1bcbf4
John T. Wodder II <git@varonathe.org>
2022-11-04 09:52:05 -0400

    Test error displays

--- src/lib.rs
+++ src/lib.rs
@@ -204,4 +204,29 @@ mod tests {
     fn test_strtoint_i32_err(#[case] s: &str, #[case] err: StrToIntError) {
         assert_eq!(strtoint::<i32>(s).unwrap_err(), err);
     }
+
+    #[test]
+    fn test_display_error_no_digits() {
+        assert_eq!(StrToIntError::NoDigits.to_string(), "no digits in input");
+    }
+
+    #[test]
+    fn test_display_error_invalid_character() {
+        assert_eq!(
+            StrToIntError::InvalidCharacter {
+                c: '.',
+                position: 2
+            }
+            .to_string(),
+            "invalid character '.' at byte position 2"
+        );
+    }
+
+    #[test]
+    fn test_display_error_out_of_range() {
+        assert_eq!(
+            StrToIntError::OutOfRange.to_string(),
+            "value is out of range for numeric type"
+        );
+    }
 }
