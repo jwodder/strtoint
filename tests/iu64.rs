ac82bac702eb674825875423072aa6404c15fbc8
John T. Wodder II <git@varonathe.org>
2022-11-04 15:28:14 -0400

    Typo

--- tests/iu64.rs
+++ tests/iu64.rs
@@ -76,6 +76,6 @@ fn test_strtoint_nonzero_u64(#[case] s: &str, #[case] x: u64) {
 #[case("-9223372036854775807", StrToIntError::InvalidCharacter {c: '-', position: 0})]
 #[case("-9223372036854775808", StrToIntError::InvalidCharacter {c: '-', position: 0})]
 #[case("18446744073709551616", StrToIntError::OutOfRange)]
-fn test_strtoint_nonzeri_u64_err(#[case] s: &str, #[case] err: StrToIntError) {
+fn test_strtoint_nonzero_u64_err(#[case] s: &str, #[case] err: StrToIntError) {
     assert_eq!(strtoint::<NonZeroU64>(s).unwrap_err(), err);
 }
